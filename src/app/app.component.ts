739372bf8c7080744ac2a80d20403eee12409a8f
blablanumerodeux <blablanumerodeux@gmail.com>
2019-04-07 00:18:36 -0400

    first request to GraphQl from Apollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,8 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   title: string = 'DaKar-ngFront';
-  favoriteSeason: string;
-  seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
 
   constructor() {
-    console.log('test');
+    console.log('app constructor');
   }
-
 }
--- src/app/components/journey/journey.component.ts
+++ src/app/components/journey/journey.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {Journey} from "../../models/Journey";
-import gql from "graphql-tag";
-import {map, shareReplay} from "rxjs/operators";
-import {Apollo} from "apollo-angular";
-import {Observable} from "rxjs";
+import {Journey} from '../../models/Journey';
+import gql from 'graphql-tag';
+import {map, shareReplay} from 'rxjs/operators';
+import {Apollo} from 'apollo-angular';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-journey',
@@ -17,32 +17,38 @@ export class JourneyComponent implements OnInit {
   loading$: Observable<boolean>;
   errors$: Observable<any>;
 
-
   constructor(private apollo: Apollo) {
-    console.log('ngOnInit');
+    console.log('journey constructor');
   }
 
   ngOnInit() {
     console.log('ngOnInit');
-    console.log('journey');
     this.journey = {
       id: 1,
       destination: 'dfdf'
     };
+
     const source$ = this.apollo.query({
       query: gql`
-        {
-          rates(currency: "USD") {
-            currency
-            rate
+        query find{
+          findJourneyById(id: "tt") {
+            destination
           }
         }
       `
     }).pipe(shareReplay(1));
 
-    this.rates$ = source$.pipe(map(result => result.data && result.data['rates']));
+    this.rates$ = source$.pipe(map(result => result.data && result.data['findJourneyById']['destination']));
+    this.rates$.subscribe(
+      (res) => {
+        this.journey.destination = res.toString();
+        console.log(res);
+      },
+      (err) => console.log(err),
+      () => console.log('done!')
+    );
+    console.log(this.rates$);
     this.loading$ = source$.pipe(map(result => result.loading));
     this.errors$ = source$.pipe(map(result => result.errors));
   }
-
 }
