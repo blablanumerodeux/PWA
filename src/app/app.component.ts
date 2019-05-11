3a2febfa1ff57bd33d439efab28f9a779125944e
blablanumerodeux <blablanumerodeux@gmail.com>
2019-05-11 12:53:17 -0400

    implements ondestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
   title = 'DaKar-ngFront';
 
