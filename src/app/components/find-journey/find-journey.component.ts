7d7dd93cc36f9e669cd6418f43cf66f0331a10df
blablanumerodeux <blablanumerodeux@gmail.com>
2019-06-15 03:22:17 -0400

    features/83_find_journey: chore: refacto of findByCriteria

--- src/app/components/find-journey/find-journey.component.ts
+++ src/app/components/find-journey/find-journey.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Journey} from '../../models/Journey';
 import {JourneyService} from '../../services/journey.service';
+import {log} from 'util';
+import {filter, tap} from 'rxjs/operators';
 
 /**
  * This component should be used to search a Journey
@@ -23,17 +25,42 @@ export class FindJourneyComponent {
   }
 
   onSubmit() {
-    console.log('id searched: ', this.id.value);
-    console.log('destination searched: ', this.destination.value);
 
+    if (this.id.value !== undefined && this.id.value.trim().length !== 0) {
+      console.log('id searched: ', this.id.value);
+      this.searchById();
+    } else if (this.destination.value !== undefined && this.destination.value.trim().length !== 0) {
+      console.log('destination searched: ', this.destination.value);
+      this.searchByCriterias();
+    }
+  }
+
+  private searchByCriterias() {
+    this.journeyService.searchByCriterias(this.destination.value)
+      .pipe(
+        tap(result => log(result)),
+        filter(res => Array.from(res).length > 0),
+        tap(result => log(result))
+      )
+      .subscribe(
+        (response) => {
+          this.journeysFound = response;
+        },
+        (err) => console.error(err),
+        () => console.log('done searching by criteria !')
+      );
+  }
+
+  private searchById() {
     this.journeyService.searchById(this.id.value)
+      .pipe(tap(result => log(result)))
       .subscribe(
         (response) => {
           this.journey = response;
           this.journeysFound = [this.journey];
         },
         (err) => console.error(err),
-        () => console.log('done!')
+        () => console.log('done searching by id !')
       );
   }
 }
